787b639a22da89e7cd99c008ab60e5e642352140
El vostre nom <usuari@domini.com>
2023-04-21 08:54:23 +0200

    com funcionen els paràmetres

---
+++ parametres.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#veurem com funcionen els paràmetres
+PARAMETRE0=${0}
+#${0} ens indica el nom de l'executable
+echo "el parametre 0 és ${0}" 
+
+#${1} és el primer paràmetre posicional
+echo "el paramtre 1 és ${1}"
+
+#podeu escriure fins a 9 (verificar)
+echo "el parametre 2 és ${2}"
+
+parametre_sostingut=${#}
+echo "el parametre sostingut ${parametre_sostingut} indica el número de paràmetres"
